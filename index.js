aacb0342c3a4f22c3c6f0b284384059ab7e072b7
Breno S F <BrenoSilvaFortunato@gmail.com>
2021-04-15 13:04:51 -0300

    commit all

---
+++ index.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergunta");
+const Resposta = require("./database/Resposta");
+
+//DATABASE
+connection
+  .authenticate()
+  .then(() => {
+    console.log("conexao feita com o banco de dados");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// ESTOU DIZENDO PARA O EXPRESS USAR O EJS COMO VIEW ENGINE
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//ROTAS
+app.get("/", (req, res) => {
+  Pergunta.findAll({ raw: true, order: [
+    ["createdAt", "DESC"]
+  ]}).then((perguntas) => {
+        res.render("index", {
+        perguntas: perguntas,
+      });
+    });
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var desc = req.body.desc;
+
+  Pergunta.create({
+    titulo: titulo,
+    descricao: desc,
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+});
+
+app.get("/pergunta/:id", (req, res) => {
+  var id = req.params.id;
+
+ 
+  Pergunta.findOne({
+    where: {id: id}
+  }).then(pergunta => {
+    if(pergunta != undefined){
+
+      Resposta.findAll({
+        where:{perguntaId: id},
+        order: [['createdAt', 'DESC']]
+      }).then(respostas => {
+        res.render("pergunta", {
+          pergunta: pergunta,
+          respostas: respostas
+        })
+      })
+    
+    }else{
+      res.redirect("/");
+    }
+  })
+})
+
+app.post("/responder", (req, res) => {
+  var perguntaId = req.body.perguntaId;
+  var corpo = req.body.corpo;
+
+  Resposta.create({
+    perguntaId: perguntaId,
+    corpo: corpo,
+  })
+    .then(() => {
+      res.redirect("/pergunta/" + perguntaId);
+    })
+});
+
+
+
+//
+app.listen(4000, () => {
+  console.log("app rodando!");
+});
